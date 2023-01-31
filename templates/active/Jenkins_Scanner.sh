3575b222b1633f255d78f57ebd99978e7748eaa3
W3 Security <84289402+W30x@users.noreply.github.com>
2023-01-30 22:00:58 -0800

    Add files via upload
    Signed-off-by: W3 Security <84289402+W30x@users.noreply.github.com>

---
+++ templates/active/Jenkins_Scanner.sh
@@ -0,0 +1,9 @@
+AUTHOR='w3security'
+VULN_NAME='Jenkins Detected'
+URI='/login?from=%2F'
+METHOD='GET'
+MATCH="\[Jenkins\]"
+SEVERITY='P5 - INFO'
+CURL_OPTS="--user-agent '' -s -L --insecure"
+SECONDARY_COMMANDS=''
+GREP_OPTIONS='-i'
---
+++ templates/active/Sitemap.xml_Detected.sh
@@ -0,0 +1,9 @@
+AUTHOR='w3security'
+VULN_NAME='Sitemap.xml Detected'
+URI='/sitemap.xml'
+METHOD='GET'
+MATCH='<?xml\ '
+SEVERITY='P5 - INFO'
+CURL_OPTS="--user-agent '' -s -L --insecure"
+SECONDARY_COMMANDS=''
+GREP_OPTIONS='-i'
